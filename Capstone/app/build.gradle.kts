f446c1cc2cf475b72bf3b7d792574a0805c0d7f5
bray-y <165206451+bray-y@users.noreply.github.com>
2025-11-14 19:21:20 -0500

    Created a very basic dashboard for the app

--- Capstone/app/build.gradle.kts
+++ Capstone/app/build.gradle.kts
@@ -41,6 +41,8 @@ android {
 
 dependencies {
 
+    implementation("com.google.android.material:material:1.10.0")
+    implementation("androidx.cardview:cardview:1.0.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -49,6 +51,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
